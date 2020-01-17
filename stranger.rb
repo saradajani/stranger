5b323964f064ed12d78776e2c75f9c5eb2d85632
saradajani <37843528+saradajani@users.noreply.github.com>
2020-01-17 14:42:56 -0800

    Add files via upload

---
+++ stranger.rb
@@ -0,0 +1,28 @@
+use_bpm 168
+use_bpm 168
+use_synth :dsaw
+live_loop :strange do
+  play :c4
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :e4
+  sleep 0.5
+  play :e3
+  sleep 0.5
+  play :g3
+  sleep 0.5
+  play :b3
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :b3
+  sleep 0.5
+  play :g3
+  sleep 0.5
+  play :e3
+  sleep 0.5
+  play :c2
+  sleep 0.5
+  play :c3
+end
